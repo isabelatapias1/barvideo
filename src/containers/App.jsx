27b60adeb6d9318bf4bf00a2eccdd315cef48f11
isabelatapias1 <77413685+isabelatapias1@users.noreply.github.com>
2021-09-06 16:43:08 -0500

    1.1

---
+++ src/containers/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
+
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+
+import '../assets/styles/App.scss'
+
+const App = ( ) => {
+    const [ videos, setVideos ] = useState({ mylist: [], trends: [], originals: [] });
+    useEffect(() => {
+        fetch('http://localhost:3000/initialState')
+        .then(response => response.json())
+        .then(data => setVideos(data));
+    }, []);
+
+    
+    return ( 
+        <div className="App"> 
+        <Header/>
+        <Search/>
+
+        {videos.mylist.length > 0 && 
+        <Categories title="albumes">
+            <Carousel>
+                <corouselItem/>
+            </Carousel>
+        </Categories>
+
+        }
+        <Categories title="Tendencias">
+        <Carousel>
+            {videos.trends.map(item =>
+            <CarouselItem key={item.id} {...item} />
+            )}
+        </Carousel>
+        </Categories>
+
+        <Footer />     
+        </div>
+        );
+            }
+
+
+
+export default App ;
+
